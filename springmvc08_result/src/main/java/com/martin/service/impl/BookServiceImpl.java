29ad2f9daaeb27ea6dd47bc4605e82e117aaed65
martinhou <martinhou@foxmail.com>
2022-07-23 15:25:41 +0800

    Show {code,data,msg} after calls.

---
+++ springmvc08_result/src/main/java/com/martin/service/impl/BookServiceImpl.java
@@ -0,0 +1,44 @@
+package com.martin.service.impl;
+
+import com.martin.dao.BookDao;
+import com.martin.domain.Book;
+import com.martin.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean save(Book book) {
+        bookDao.save(book);
+        return true;
+    }
+
+    @Override
+    public boolean update(Book book) {
+        bookDao.update(book);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        bookDao.delete(id);
+        return true;
+    }
+
+    @Override
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
