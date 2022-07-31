31e448be3d3f2165cd78fa8cff32a162043aa41b
martinhou <martinhou@foxmail.com>
2022-07-31 20:21:22 +0800

    简化：将SpringMvcConfig实现WebMvcConfigurer，来替代原SpringMvcSupport中的功能。但是侵入性强。

--- springmvc11_interceptor/src/main/java/com/martin/config/SpringMvcConfig.java
+++ springmvc11_interceptor/src/main/java/com/martin/config/SpringMvcConfig.java
@@ -1,11 +1,29 @@
 package com.martin.config;
 
+import com.martin.controller.interceptor.ProjectInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan({"com.martin.controller","com.martin.config"})
+@ComponentScan("com.martin.controller")
 @EnableWebMvc
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ProjectInterceptor interceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/books","/books/*"); //不仅拦截/books,还拦/books/xxx
+    }
 }
