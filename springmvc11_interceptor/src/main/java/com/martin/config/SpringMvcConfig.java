d79fba3f502ea5ff06887308ff8e730ff8ae5e46
martinhou <martinhou@foxmail.com>
2022-08-12 21:12:07 +0800

    springboot添加

--- springmvc11_interceptor/src/main/java/com/martin/config/SpringMvcConfig.java
+++ springmvc11_interceptor/src/main/java/com/martin/config/SpringMvcConfig.java
@@ -1,6 +1,7 @@
 package com.martin.config;
 
 import com.martin.controller.interceptor.ProjectInterceptor;
+import com.martin.controller.interceptor.ProjectInterceptor2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +18,22 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     @Autowired
     private ProjectInterceptor interceptor;
 
+    @Autowired
+    private ProjectInterceptor2 interceptor2;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/pages/**").addResourceLocations("/pages/");
     }
 
+    /*
+        多拦截器下，按registry顺序为正序，pre handler以正序执行，post和after competition按倒序。
+        如果某一pre handler返回false，则其后面所有都不执行，直接跳到对应after completion后面再执行。
+        比如 pre handler2返回false，则下一个应执行after completion1而不是2。
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/books","/books/*"); //不仅拦截/books,还拦/books/xxx
+        registry.addInterceptor(interceptor2).addPathPatterns("/books","/books/*"); //不仅拦截/books,还拦/books/xxx
     }
 }
---
+++ springmvc11_interceptor/src/main/java/com/martin/controller/interceptor/ProjectInterceptor2.java
@@ -0,0 +1,37 @@
+package com.martin.controller.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class ProjectInterceptor2 implements HandlerInterceptor {
+    // 前置拦截器
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String header = request.getHeader("Content-Type");  // 可用接口
+
+        HandlerMethod handlerMethod = (HandlerMethod) handler;  // 反射得到调用的Controller类
+        String name = handlerMethod.getMethod().getName();
+
+        System.out.println("pre handler2 " + header + " " + name);
+
+        return true; //false则不执行原始操作，到这里就结束了
+    }
+
+    //后置拦截器
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("post handler2");
+    }
+
+    //完成后的拦截器（异常等）
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("after completion2");
+    }
+}
