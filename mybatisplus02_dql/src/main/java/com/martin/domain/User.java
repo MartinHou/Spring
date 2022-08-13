976bc4b54ace8af0c3f3db57df82c58afa0d5921
martinhou <martinhou@foxmail.com>
2022-08-13 17:21:39 +0800

    java与数据库映射关系管理

--- mybatisplus02_dql/src/main/java/com/martin/domain/User.java
+++ mybatisplus02_dql/src/main/java/com/martin/domain/User.java
@@ -1,5 +1,7 @@
 package com.martin.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 //@Setter
@@ -8,10 +10,16 @@ import lombok.Data;
 //@EqualsAndHashCode
 //上面四个和下面这个等效
 @Data
+@TableName("t_user")    // 映射到表名
 public class User {
+    @TableField(value = "id")   // 映射到数据库表的字段名
     private Long id;
     private String name;
+    @TableField(select = false) // 不参与查询
     private String password;
     private Integer age;
     private String tel;
+
+    @TableField(exist = false)
+    private boolean online; // 不在表中有，只在java类中使用
 }
